e0bc1339af201599d7aaa854cfa60f88b556d26a
hestu14 <61925354+hestu14@users.noreply.github.com>
2020-03-31 18:15:31 +0700

    ver 1.1
    Update death rate & ubah class column menjadi 4

--- script.js
+++ script.js
@@ -18,10 +18,23 @@ let bulan = months[b];
 
 $("#date").html(d.getDate() + " " + bulan + " " + d.getFullYear());
     $.ajax({url: "https://api.kawalcorona.com/indonesia/", success: function(result){
-     $("#terjangkit").html(result[0].positif);
-     $("#sembuh").html(result[0].sembuh);
-     $("#meninggal").html(result[0].meninggal);
+    var positif =  result[0].positif;
+    var positif2 = positif.split(",");
+    var positif3 = positif2.join("");
+
+    var sembuh = result[0].sembuh;
+    var meninggal = result[0].meninggal;
+
+    $("#terjangkit").html(positif);
+    $("#sembuh").html(sembuh);
+    $("#meninggal").html(meninggal);
+    
+    let rate = meninggal / positif3 * 100;
+    let rate2 = rate.toFixed(2);
+    $("#rate").html(rate2);
+
     }});
 });
+
 
 
